2d0e2fcac23f159f47b7d024ecbc63ef622cd8d7
ZhangGaoxing <993638176@qq.com>
2016-12-25 10:27:36 +0800

    HamburgerDemo
    UWP Hamburger Menu Demo

---
+++ HamburgerDemo/HamburgerDemo/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using HamburgerDemo.Views;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace HamburgerDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        // 为不同的菜单创建不同的List类型
+        private List<NavMenuItem> navMenuPrimaryItem = new List<NavMenuItem>(
+            new[]
+            {
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE10F",
+                    Label = "页面1",
+                    Selected = Visibility.Visible,
+                    DestPage = typeof(Page1)
+                },
+
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE11A",
+                    Label = "页面2",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(Page1)
+                },
+
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE121",
+                    Label = "页面3",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(Page1)
+                },
+
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE122",
+                    Label = "页面4",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(Page1)
+                }
+
+            });
+
+        private List<NavMenuItem> navMenuSecondaryItem = new List<NavMenuItem>(
+            new[]
+            {
+                new NavMenuItem()
+                {
+                    FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                    Icon = "\xE713",
+                    Label = "设置",
+                    Selected = Visibility.Collapsed,
+                    DestPage = typeof(Page1)
+                }
+            });
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // 绑定导航菜单
+            NavMenuPrimaryListView.ItemsSource = navMenuPrimaryItem;
+            NavMenuSecondaryListView.ItemsSource = navMenuSecondaryItem;
+            // SplitView 开关
+            PaneOpenButton.Click += (sender, args) =>
+            {
+                RootSplitView.IsPaneOpen = !RootSplitView.IsPaneOpen;
+            };
+            // 导航事件
+            NavMenuPrimaryListView.ItemClick += NavMenuListView_ItemClick;
+            NavMenuSecondaryListView.ItemClick += NavMenuListView_ItemClick;
+            // 默认页
+            RootFrame.SourcePageType = typeof(Page1);
+        }
+
+        private void NavMenuListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 遍历，将选中Rectangle隐藏
+            foreach (var np in navMenuPrimaryItem)
+            {
+                np.Selected = Visibility.Collapsed;
+            }
+            foreach (var ns in navMenuSecondaryItem)
+            {
+                ns.Selected = Visibility.Collapsed;
+            }
+
+            NavMenuItem item = e.ClickedItem as NavMenuItem;
+            // Rectangle显示并导航
+            item.Selected = Visibility.Visible;
+            if (item.DestPage != null)
+            {
+                RootFrame.Navigate(item.DestPage);
+            }
+
+            RootSplitView.IsPaneOpen = false;
+        }
+    }
+}
---
+++ HamburgerDemo/HamburgerDemo/NavMenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace HamburgerDemo
+{
+    class NavMenuItem : INotifyPropertyChanged
+    {
+        // 记录图标字体
+        public FontFamily FontFamily { get; set; }
+        // 图标的C#转义代码
+        public string Icon { get; set; }
+        // 标题
+        public string Label { get; set; }
+        // 导航页
+        public Type DestPage { get; set; }
+        // 用于左侧矩形的显示
+        private Visibility selected = Visibility.Collapsed;
+        public Visibility Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                this.OnPropertyChanged("Selected");
+            }
+        }
+        // 双向绑定，用于更新矩形是否显示
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
